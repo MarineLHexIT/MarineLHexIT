0b58a8f5c6743cbf7f78024393c4c5f301224787
Marine Lancien (Hexagone IT) <marine.lancien@hexagone-it.fr>
2024-03-01 17:18:42 +0100

    Ajout de sort/filter

--- app/javascript/components/Beers/index.jsx
+++ app/javascript/components/Beers/index.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {Button, Popconfirm, Table} from "antd";
+import _ from "underscore"
 
 export default () => {
 
@@ -13,22 +14,43 @@ export default () => {
         {
             title: "Name",
             dataIndex: "name",
-            key: "name"
+            key: "name",
+            sorter: (a, b) => a.name.localeCompare(b.name)
         },
         {
             title: "Brand",
             dataIndex: "brand",
-            key: "brand"
+            key: "brand",
+            sorter: (a, b) => a.brand.localeCompare(b.brand)
         },
         {
             title: "Country",
             dataIndex: "country",
-            key: "country"
+            key: "country",
+            sorter: (a, b) => a.country.localeCompare(b.country),
+            filters: _.uniq(beers.map(({country}) => country)).map((country) => ({text: country, value: country})),
+            onFilter: (value, record) => record.country == value
         },
         {
             title: "Style",
             dataIndex: "style",
-            key: "style"
+            key: "style",
+            sorter: (a, b) => a.style.localeCompare(b.style)
+        },
+        {
+            title: "",
+            key: "action",
+            render: (_, record) => (
+                <Popconfirm
+                    title="Are you sure you want to delete that beer?"
+                    onConfirm={() => deleteBeer(record.id)}
+                    okText="Yes"
+                    okType="danger"
+                    cancelText="No"
+                    >
+                    <Button type="primary" danger>Delete</Button>
+                </Popconfirm>
+            )
         }
     ];
 
